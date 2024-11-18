5d362cd7314c71d2b21dcd596a325f666a19605d
max-sliva <persimon@inbox.ru>
2024-11-18 18:38:49 +0500

    code optimization

--- src/main/kotlin/SimpleEx.kt
+++ src/main/kotlin/SimpleEx.kt
@@ -1,8 +1,6 @@
 import com.formdev.flatlaf.FlatLightLaf
 import java.awt.*
-import java.awt.event.ActionEvent
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
+import java.awt.event.*
 import java.io.*
 import java.util.*
 import javax.swing.*
